2de565e247b524ef6bf35ad12eb255f9b8e4ef36
harshil sharma <harshilsharmaa0@gmail.com>
2022-05-25 14:46:23 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Equipment-wise Detail</h1>
+      <table >
+        <thead>
+          <tr>
+            <th>Machine Name</th>
+            <th>Machine ID</th>
+            <th colSpan="3">Energy consumed</th>  
+            <th>Active Power</th>
+            <th>Aprent Power</th>
+            <th>Reactive Power</th>
+            <th>CURRENT</th>
+            <th>VOLTAGE</th>
+            <th>POWER FACTOR</th>
+            <th colSpan="3">IDLE TIME</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(item => (
+            <tr key={item.machine_id}>
+              <td>{item.machine_name}</td>
+              <td>{item.machine_id}</td>
+              <td>{item.current}</td>
+              <td>{item.daily_energy}</td>
+              <td>{item.monthly_energy}</td>
+              <td>{item.yearly_energy}</td>
+              <td>{item.active_power}</td>
+              <td>{item.apparent_power}</td>
+              <td>{item.reactive_power}</td>
+              <td>{item.voltage}</td>
+              <td>{item.power_factor}</td>
+              <td>{item.idle_daily}</td>
+              <td>{item.idle_monthly}</td>
+              <td>{item.idle_yearly}</td>
+              
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
